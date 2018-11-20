2b83c8400d22d0e4698155ce3c45b5cb4ffa7adc
Moisés Maldaner de Lima <moiseslimax@gmail.com>
2018-11-20 07:02:40 -0200

    prepare to deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://admin:admin123@ds241723.mlab.com:41723/devconnector',
-    secretOrKey: 'secret'
+if (process.env.NODE_ENV === 'production') {
+    module.exports= require('./keys_prod');
+} else {
+    module.exports= require('./keys_dev');
 }
